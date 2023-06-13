5fe97e47bc90290735e1071d3089270899903c14
Shruthi Tuplur <shruthituplur@gmail.com>
2023-06-12 20:43:48 -0700

    got rid of browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {useLayoutEffect, useState, useRef, useEffect} from 'react';
 import ReactDom, {render} from 'react-dom'
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from './components/header';
 import HomepageMain from './components/homepage-main';
@@ -16,28 +16,24 @@ const Main = () => {
     
 
     return (
-        <BrowserRouter>
+        
         <div>
             <Header boogie = {boogie} setBoogie = {setBoogie} businessMode={businessMode} setBusinessMode={setBusinessMode}/>
-            <Switch>
-                <Route exact path='/' render={() => {
-                    return (
-                        <Redirect to='/home' />
-                    )
-                }} 
-                />
-                <Route exact path='/home'>
+            
+                
+                    
+                
                     <HomepageMain boogie = {boogie} setBoogie = {setBoogie} businessMode={businessMode} setBusinessMode={setBusinessMode}/>
-                </Route>
                 
                 
-            </Switch>
+                
+           
             
 
             
             <Footer businessMode={businessMode}/> 
         </div>
-        </BrowserRouter>
+        
     )
 }
 
